58eb8e451678835d25d46f83019f5251c7342c8e
Mika1212 <ilravgaz03@mail.ru>
2026-01-03 21:21:37 +0300

    feat: task kafka configs iter1

--- task-service/src/main/java/taskmanager/config/AbstractKafkaConsumerConfig.java
+++ task-service/src/main/java/taskmanager/config/AbstractKafkaConsumerConfig.java
@@ -2,7 +2,6 @@ package taskmanager.config;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer;
@@ -12,23 +11,33 @@ import java.util.HashMap;
 import java.util.Map;
 
 public abstract class AbstractKafkaConsumerConfig {
-    @Value("${KAFKA_HOST:localhost}")
-    private String kafkaHost;
 
-    @Value("${KAFKA_PORT:9092}")
-    private String kafkaPort;
+    protected final KafkaProperties kafkaProperties;
 
-    protected <T> ConsumerFactory<String, T> createConsumerFactory(String groupId, String type) {
+    protected AbstractKafkaConsumerConfig(KafkaProperties kafkaProperties) {
+        this.kafkaProperties = kafkaProperties;
+    }
+
+    protected Map<String, Object> baseConsumerProps(String groupId) {
         Map<String, Object> props = new HashMap<>();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:9092");
+
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaProperties.getBootstrapServers());
         props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, ErrorHandlingDeserializer.class);
         props.put(ErrorHandlingDeserializer.VALUE_DESERIALIZER_CLASS, JsonDeserializer.class);
-        props.put(JsonDeserializer.VALUE_DEFAULT_TYPE, type);
         props.put(JsonDeserializer.TRUSTED_PACKAGES, "*");
         props.put(JsonDeserializer.USE_TYPE_INFO_HEADERS, false);
 
-        return new DefaultKafkaConsumerFactory<>(props);
+        return props;
+    }
+
+    protected <T> ConsumerFactory<String, T> createConsumerFactory(String groupId, Class<T> eventClass) {
+        Map<String, Object> props = new HashMap<>(baseConsumerProps(groupId));
+
+        JsonDeserializer<T> deserializer = new JsonDeserializer<>(eventClass);
+        deserializer.addTrustedPackages("*");
+
+        return new DefaultKafkaConsumerFactory<>(props, new StringDeserializer(), deserializer);
     }
 }
--- task-service/src/main/java/taskmanager/config/ProjectKafkaConsumerConfig.java
+++ task-service/src/main/java/taskmanager/config/ProjectKafkaConsumerConfig.java
@@ -1,33 +1,33 @@
 package taskmanager.config;
 
-import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
-import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
-import org.springframework.kafka.support.serializer.JsonDeserializer;
 import taskmanager.common.event.ProjectCreatedEvent;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @Configuration
 public class ProjectKafkaConsumerConfig extends AbstractKafkaConsumerConfig {
 
-    @Bean
-    public ConsumerFactory<String, ProjectCreatedEvent> projectCreatedConsumerFactory() {
-        JsonDeserializer<ProjectCreatedEvent> deserializer = new JsonDeserializer<>(ProjectCreatedEvent.class);
-        deserializer.addTrustedPackages("*");
-
-        Map<String, Object> props = new HashMap<>(kafkaProps()); // метод с bootstrap-servers и др.
-        return new DefaultKafkaConsumerFactory<>(props, new StringDeserializer(), deserializer);
+    public ProjectKafkaConsumerConfig(KafkaProperties kafkaProperties) {
+        super(kafkaProperties);
     }
 
     @Bean
-    public ConcurrentKafkaListenerContainerFactory<String, ProjectCreatedEvent> projectCreatedKafkaListenerContainerFactory() {
+    public ConsumerFactory<String, ProjectCreatedEvent> projectCreatedConsumerFactory() {
+        return createConsumerFactory(
+                "project-created-group",
+                ProjectCreatedEvent.class
+        );
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, ProjectCreatedEvent>
+    projectCreatedKafkaListenerContainerFactory() {
+
         ConcurrentKafkaListenerContainerFactory<String, ProjectCreatedEvent> factory =
                 new ConcurrentKafkaListenerContainerFactory<>();
+
         factory.setConsumerFactory(projectCreatedConsumerFactory());
         return factory;
     }
