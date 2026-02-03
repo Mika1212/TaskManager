aba94063a93a4a03935ffa05b2f909c3386633cd
Mika1212 <ilravgaz03@mail.ru>
2026-02-03 21:08:46 +0300

    feat: added tests for auth controller

--- user-service/src/main/java/taskmanager/config/UserKafkaProducerConfig.java
+++ user-service/src/main/java/taskmanager/config/UserKafkaProducerConfig.java
@@ -4,6 +4,7 @@ import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
@@ -13,6 +14,7 @@ import taskmanager.common.event.UserCreatedEvent;
 import java.util.HashMap;
 import java.util.Map;
 
+@Profile("!test")
 @Configuration
 public class UserKafkaProducerConfig {
 
